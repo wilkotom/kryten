a43c8ae6752938a9b528301ed07ba985ae4f6ed0
Tom Wilkinson <twilkinson@wilkinson-rowe.name>
2019-06-20 07:23:17 +0100

    Added base energy tariff class

---
+++ kryten/datasources/energy/energy_tariff.py
@@ -0,0 +1,5 @@
+from ...exceptions import KrytenOperationNotImplemented
+
+class EnergyTariff:
+    def __init__(self):
+        raise KrytenOperationNotImplemented
--- kryten/sessions/session.py
+++ kryten/sessions/session.py
@@ -3,6 +3,8 @@ from ..exceptions import KrytenOperationNotImplemented
 
 class Session:
 
+    session_id: str = None;
+
     def __init__(self):
         raise KrytenOperationNotImplemented
 
